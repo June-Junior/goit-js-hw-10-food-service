bdef8366915c95b2a1204c47d3f62d0b3c291f5b
June-Junior <artknownuntist@gmail.com>
2020-11-08 05:36:16 +0200

    Commit 2 + webpack 4.44.2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 console.log('Это index.js');
+const a = 2;
+console.log(a);
+
+class Hero {
+    constructor(name) {
+        this._name = name;
+    }
+    get name() {
+        return this._name;
+    }
+}
+const b = new Hero('Jason');
